86af7465cdc68a7efdab11b06883c96e880ba5b3
naydenovAnton <naydenov.anton@gmail.com>
2025-10-15 16:51:54 +0300

    Remove TODO component

--- src/App.vue
+++ src/App.vue
@@ -1,10 +1,8 @@
 <script setup lang="ts">
 
-
 import { Authenticator } from "@aws-amplify/ui-vue";
 import "@aws-amplify/ui-vue/styles.css";
 
-import Todos from './components/Todos.vue'
 </script>
 
 <template>
